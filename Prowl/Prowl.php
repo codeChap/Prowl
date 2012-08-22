19dfb97475f417a7d1db68094f4eeb27652487cd
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2012-08-22 10:18:23 +0200

    setup files and folders

---
+++ Prowl/Prowl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * PHP library to access the Prowl service.
+ *
+ * @author     Derrick Egersdorfer
+ * @license    MIT License
+ * @copyright  2011 - 2012 Derrick Egersdorfer
+ */
+
+namespace Prowl;
+
+class Parser
+{
+    public function push()
+    {
+
+    }
+}
