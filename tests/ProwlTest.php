505e8a294a9d196a82bdcc43fd7b580329827b36
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2012-09-19 13:34:11 +0200

    tests

--- tests/ProwlTest.php
+++ tests/ProwlTest.php
@@ -2,9 +2,55 @@
 
 	class ProwlTest extends PHPUnit_Framework_TestCase {
 
-		function test()
+		function test_a()
 		{
-			return true;
+			$conf = array(
+				'application' => 'application',
+				'key' => '1234567890123456789012345678901234567890',
+				'failOnNotAuthorized' => false,
+				'subject' => 'subject',
+				'message' => 'test a',
+				'action' => 'http://example.com',
+				'priority' => 2,
+				'url' => "https://api.prowlapp.com/publicapi/add"
+			);
+
+			$p = new Prowl($conf);
+			$p->push();
 		}
 
+		function test_b()
+		{
+			$conf = array(
+				'application' => 'application',
+				'key' => '1234567890123456789012345678901234567890',
+				'failOnNotAuthorized' => false,
+				'subject' => 'subject'
+			);
+
+			$p = new Prowl($conf);
+			$p->push('test b');
+		}
+
+		function test_c()
+		{
+			$p = new Prowl();
+			$p->setApplication('application');
+			$p->setKey('1234567890123456789012345678901234567890');
+			$p->setFailOnNotAuthorized(false),
+			$p->setSubject('subject'),
+			$p->setMessage('test c')
+			$p->push();
+		}
+
+		function test_d()
+		{
+			$p = new Prowl();
+			$p->setApplication('application');
+			$p->setKey('1234567890123456789012345678901234567890');
+			$p->setFailOnNotAuthorized(false),
+			$p->setSubject('subject'),
+			$p->setMessage('test c')
+			$p->push();
+		}
 	}
