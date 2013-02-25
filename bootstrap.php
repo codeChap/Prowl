3281444053db4dabb239ec1ef86ce1ea92c6cbcc
Derrick Egersdorfer <derrick@egersdorfer.co.za>
2013-02-25 13:42:34 +0200

    moved to PSR

--- bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Autoloader::add_core_namespace('Prowl');
-
-Autoloader::add_classes(array(
-	'Prowl\\Prowl' => __DIR__.'/classes/prowl.php'
-));
--- config/prowl.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-		'key' => '2658ef9889d7bad3f79006a89b239e13b3700192',
-		'url' => 'https://api.prowlapp.com/publicapi/add',
-		'application' => 'application',
-		'subject' => 'subject',
-		'description' => 'description'
-	);
